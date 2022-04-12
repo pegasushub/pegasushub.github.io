770149b413887e045753d4a792e5de952a0b8682
aditi1208 <aditi.jain56@gmail.com>
2022-04-12 14:00:47 -0700

    PM-1854, PM-1852: Added token back in process.py

--- scripts/process.py
+++ scripts/process.py
@@ -20,11 +20,11 @@ logging.basicConfig(filename="logs/logs.txt", format="%(asctime)s %(levelname)s 
 logger = logging.getLogger(__name__)
 
 workflows = []
-# headers = {
-#     'Accept': 'application/vnd.github.mercy-preview+json',
-#     'Authorization': 'token {}'.format(os.environ['PEGASUSHUB_TOKEN'])
-# }
-headers = {}
+headers = {
+    'Accept': 'application/vnd.github.mercy-preview+json',
+    'Authorization': 'token {}'.format(os.environ['PEGASUSHUB_TOKEN'])
+}
+# headers = {}
 
 config_schema = Schema({
     "organization": str,
@@ -90,99 +90,99 @@ for error in errors:
     else:
         print(error.message)
 
-# for w in workflows:    
-#     url = 'https://api.github.com/repos/{}/{}'.format(w['organization'], w['repo_name'])
-#     r = requests.get(url, headers=headers)
-#     if r.status_code == 404:
-#         #repo has been deleted / not found / is private
-#         dt = datetime.datetime.now()
-#         curr_time = dt.strftime("%d/%m/%Y %H:%M:%S")
-#         logger.warning(f"{w['repo_name']} has been deleted or made private from {w['organization']}")
-#         continue
+for w in workflows:    
+    url = 'https://api.github.com/repos/{}/{}'.format(w['organization'], w['repo_name'])
+    r = requests.get(url, headers=headers)
+    if r.status_code == 404:
+        #repo has been deleted / not found / is private
+        dt = datetime.datetime.now()
+        curr_time = dt.strftime("%d/%m/%Y %H:%M:%S")
+        logger.warning(f"{w['repo_name']} has been deleted or made private from {w['organization']}")
+        continue
 
-#     response = r.json()
-#     logger.info(f"{w['repo_name']} is being populated\n")
-#     # repo general information
-#     w['title'] = response['name']
-#     w['default_branch'] = response['default_branch']
-#     w['subtitle'] = response['description']
-#     w['license'] = response['license']['name'] if response['license'] else 'No licence available'
-#     w['issues'] = response['open_issues']
-#     w['forks'] = response['forks']
-#     w['stargazers'] = response['stargazers_count']
+    response = r.json()
+    logger.info(f"{w['repo_name']} is being populated\n")
+    # repo general information
+    w['title'] = response['name']
+    w['default_branch'] = response['default_branch']
+    w['subtitle'] = response['description']
+    w['license'] = response['license']['name'] if response['license'] else 'No licence available'
+    w['issues'] = response['open_issues']
+    w['forks'] = response['forks']
+    w['stargazers'] = response['stargazers_count']
 
-#     # date
-#     date = datetime.datetime.strptime(response['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
-#     w['year'] = date.strftime('%Y')
-#     w['month'] = date.strftime('%b')
-#     w['monthn'] = date.strftime('%m')
-#     w['day'] = date.strftime('%d')
+    # date
+    date = datetime.datetime.strptime(response['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
+    w['year'] = date.strftime('%Y')
+    w['month'] = date.strftime('%b')
+    w['monthn'] = date.strftime('%m')
+    w['day'] = date.strftime('%d')
 
-#     # topics
-#     w['topics'] = 'No topics available'
-#     w['tags'] = ''
-#     topics = response['topics']
-#     if len(topics) > 0:
-#         w['topics'] = ''
-#         for topic in topics:
-#             w['topics'] += '<span class="topic">{}</span>&nbsp;&nbsp;'.format(topic);
-#             w['tags'] += '{} '.format(topic)
+    # topics
+    w['topics'] = 'No topics available'
+    w['tags'] = ''
+    topics = response['topics']
+    if len(topics) > 0:
+        w['topics'] = ''
+        for topic in topics:
+            w['topics'] += '<span class="topic">{}</span>&nbsp;&nbsp;'.format(topic);
+            w['tags'] += '{} '.format(topic)
 
-#     # releases
-#     url = response['releases_url']
-#     r = requests.get(url[:-5], headers=headers)
-#     data = r.json()
-#     w['release'] = 'No release available'
-#     w['releases'] = len(data)
-#     if len(data) > 0:
-#         date = datetime.datetime.strptime(data[0]['published_at'], '%Y-%m-%dT%H:%M:%SZ')
-#         release_date = date.strftime('%d %b %Y')
-#         w['release'] = '<a href="{}" target="_blank">{}</a> <span class="release-date">({})</span>'.format(data[0]['html_url'], data[0]['name'], release_date)
+    # releases
+    url = response['releases_url']
+    r = requests.get(url[:-5], headers=headers)
+    data = r.json()
+    w['release'] = 'No release available'
+    w['releases'] = len(data)
+    if len(data) > 0:
+        date = datetime.datetime.strptime(data[0]['published_at'], '%Y-%m-%dT%H:%M:%SZ')
+        release_date = date.strftime('%d %b %Y')
+        w['release'] = '<a href="{}" target="_blank">{}</a> <span class="release-date">({})</span>'.format(data[0]['html_url'], data[0]['name'], release_date)
 
-#     # contributors
-#     r = requests.get(response['contributors_url'], headers=headers)
-#     data = r.json()
-#     w['contributors'] = len(data)
-#     w['contributors_list'] = ''
-#     for c in data:
-#         w['contributors_list'] += '<a href="{}" target="_blank"><img src="{}" width="32" height="32"/></a>&nbsp;&nbsp;'.format(c['html_url'], c['avatar_url'])
+    # contributors
+    r = requests.get(response['contributors_url'], headers=headers)
+    data = r.json()
+    w['contributors'] = len(data)
+    w['contributors_list'] = ''
+    for c in data:
+        w['contributors_list'] += '<a href="{}" target="_blank"><img src="{}" width="32" height="32"/></a>&nbsp;&nbsp;'.format(c['html_url'], c['avatar_url'])
 
-#     # metadata file
-#     r = requests.get('https://raw.githubusercontent.com/{}/{}/master/.pegasushub.yml'.format(w['organization'], w['repo_name']))
-#     w['pegasus_version'] = 'No version information available'
-#     w['dependencies'] = 'No dependencies information available'
-#     if r.ok:
-#         data = yaml.safe_load(r.text)
-#         try:
-#             w['pegasus_version'] = data['pegasus']['version']['min'] 
-#             if data['pegasus']['version']['min'] != data['pegasus']['version']['max']:
-#                 w['pegasus_version'] = '[{}, {}]'.format(data['pegasus']['version']['min'], data['pegasus']['version']['max'])
-#         except:
-#             pass
+    # metadata file
+    r = requests.get('https://raw.githubusercontent.com/{}/{}/master/.pegasushub.yml'.format(w['organization'], w['repo_name']))
+    w['pegasus_version'] = 'No version information available'
+    w['dependencies'] = 'No dependencies information available'
+    if r.ok:
+        data = yaml.safe_load(r.text)
+        try:
+            w['pegasus_version'] = data['pegasus']['version']['min'] 
+            if data['pegasus']['version']['min'] != data['pegasus']['version']['max']:
+                w['pegasus_version'] = '[{}, {}]'.format(data['pegasus']['version']['min'], data['pegasus']['version']['max'])
+        except:
+            pass
 
-#         try:
-#             w['dependencies'] = ''
-#             for dep in data['dependencies']:
-#                 if w['dependencies']:
-#                     w['dependencies'] += ', '
-#                 w['dependencies'] += dep
-#         except:
-#             w['dependencies'] = 'No dependencies information available'
+        try:
+            w['dependencies'] = ''
+            for dep in data['dependencies']:
+                if w['dependencies']:
+                    w['dependencies'] += ', '
+                w['dependencies'] += dep
+        except:
+            w['dependencies'] = 'No dependencies information available'
 
-#     # metadata information
-#     if 'training' not in w:
-#         w['training'] = 'False'
-#     if 'highlight' not in w:
-#         w['highlight'] = 'False'
-#     if 'priority' not in w:
-#         w['priority'] = 0
-#     if 'execution_sites' not in w:
-#         w['execution_sites'] = []
+    # metadata information
+    if 'training' not in w:
+        w['training'] = 'False'
+    if 'highlight' not in w:
+        w['highlight'] = 'False'
+    if 'priority' not in w:
+        w['priority'] = 0
+    if 'execution_sites' not in w:
+        w['execution_sites'] = []
 
-#     with open('scripts/workflow.html.in') as f:
-#         template = Template(f.read())
-#         contents = template.substitute(w)
+    with open('scripts/workflow.html.in') as f:
+        template = Template(f.read())
+        contents = template.substitute(w)
 
-#         # write workflows data
-#         with open('_workflows/{}-{}.html'.format(w['organization'], w['repo_name']), 'w') as f:
-#             f.write(contents)
+        # write workflows data
+        with open('_workflows/{}-{}.html'.format(w['organization'], w['repo_name']), 'w') as f:
+            f.write(contents)
